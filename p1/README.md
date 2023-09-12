e8064cce03706dbd74ebf5f3803007fbc6886dea
Susan Buck <susanbuck@fas.harvard.edu>
2023-09-12 21:31:05 +0000

    Started P1

---
+++ p1/README.md
@@ -0,0 +1,10 @@
+# Project 1
++ By: Susan Buck
++ URL: <http://e2p1.hesweb.xyz>
+
+## Game planning
+
+## Outside resources
+
+## Notes for instructor
+n/a
---
+++ p1/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang='en'>
+
+<head>
+
+    <title>Project 1</title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+
+</head>
+
+<body>
+    <h1>Project 1</h1>
+
+    <h2>Game Mechanics</h2>
+    <ul>
+        <li>...</li>
+    </ul>
+
+    <h2>Results</h2>
+    <ul>
+        <li>...</li>
+    </ul>
+
+
+</body>
+
+</html>
