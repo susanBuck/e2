98508176c15bdc52ecd1622a04b89173150e4aa3
Susan Buck <susanbuck@fas.harvard.edu>
2023-09-19 15:40:06 +0000

    Week 3 progress as of Part 6 video

---
+++ practice/bank-view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+
+    <title>PHPiggy Bank</title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+
+</head>
+
+<body>
+
+    <img alt='PHPiggy Bank Logo' src='https://s3.amazonaws.com/making-the-internet/php-piggy-bank-logo@2x.png' style='width:202px;'>
+
+    <p>
+        You have $<?php echo $total ?> in your piggy bank.
+    </p>
+
+    You have <?php echo $quarter; ?> quarters in your bank.
+
+</body>
+
+</html>
